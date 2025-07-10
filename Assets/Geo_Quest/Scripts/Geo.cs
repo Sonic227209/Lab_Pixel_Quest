1679145130223784ff7cbf3bf94d1649442b0aeb
Sonic227209 <gabrielsanchez0913@gmail.com>
2025-07-10 12:16:52 -0400

    Class 4

--- Assets/Geo_Quest/Scripts/Geo.cs
+++ Assets/Geo_Quest/Scripts/Geo.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class Geo : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public int speed = 5;
     int varTwo = 3;
-    private int varOne = 1;
-    private string bear = "How are you"; 
-    // Start is called before: the first frame update
+
+ 
+
     void Start()
     {
-      
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.left;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(varTwo);
-        varTwo++;
+        float Xinput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(Xinput * speed, rb.velocity.y);
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
         
     }
 }
