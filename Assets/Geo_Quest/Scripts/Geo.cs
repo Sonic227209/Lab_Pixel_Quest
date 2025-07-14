ae04be81a86fb64efe2a23cce46f08cb4f2d7b13
Sonic227209 <gabrielsanchez0913@gmail.com>
2025-07-14 12:14:58 -0400

    class 5

--- Assets/Geo_Quest/Scripts/Geo.cs
+++ Assets/Geo_Quest/Scripts/Geo.cs
@@ -8,7 +8,7 @@ public class Geo : MonoBehaviour
     public int speed = 5;
     int varTwo = 3;
 
- 
+
 
     void Start()
     {
@@ -16,28 +16,40 @@ public class Geo : MonoBehaviour
         rb.velocity = Vector2.left;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        float Xinput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(Xinput * speed, rb.velocity.y);
-        
-        if (Input.GetKeyDown(KeyCode.W))
+        Debug.Log(collision.tag);
+        switch (collision.tag)
         {
-            transform.position += new Vector3(0, 1, 0);
+            case "Win":
+                {
+                    break;
+                }
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        // Update is called once per frame
+        void Update()
         {
-            transform.position += new Vector3(0, -1, 0);
+            float Xinput = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector2(Xinput * speed, rb.velocity.y);
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                transform.position += new Vector3(0, 1, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                transform.position += new Vector3(0, -1, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.position += new Vector3(-1, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.position += new Vector3(1, 0, 0);
-        }
-        
+
     }
 }
+     
