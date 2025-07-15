e2b539ce6100f42ad4536125a1554e644465a9b6
Sonic227209 <gabrielsanchez0913@gmail.com>
2025-07-15 14:15:21 -0400

    class 6

--- Assets/Geo_Quest/Scripts/Geo.cs
+++ Assets/Geo_Quest/Scripts/Geo.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Geo : MonoBehaviour
 {
     private Rigidbody2D rb;
     public int speed = 5;
     int varTwo = 3;
-
+    public string nextLevel = "Scene_2";
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.left;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,35 +21,31 @@ public class Geo : MonoBehaviour
         Debug.Log(collision.tag);
         switch (collision.tag)
         {
-            case "Win":
+            case "Respawn":
                 {
+                    string thisLevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thisLevel);
+                    break;
+                }
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel);
                     break;
                 }
         }
-        // Update is called once per frame
-        void Update()
-        {
-            float Xinput = Input.GetAxis("Horizontal");
-            rb.velocity = new Vector2(Xinput * speed, rb.velocity.y);
-
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                transform.position += new Vector3(0, 1, 0);
-            }
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                transform.position += new Vector3(0, -1, 0);
-            }
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                transform.position += new Vector3(-1, 0, 0);
-            }
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                transform.position += new Vector3(1, 0, 0);
-            }
-        }
+       
 
     }
+    // Update is called once per frame
+    void Update()
+    {
+        float Xinput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(Xinput * speed, rb.velocity.y);
+
+
+    }
+
+
+
 }
      
